aa05d49ef6e7cfb848b155ba9b2269fd8c905714
Gogoonbuntu <tmddud333@naver.com>
2024-08-25 08:08:17 +0900

    theme 디비 연동 완료

--- src/client/RequestQueryClient.js
+++ src/client/RequestQueryClient.js
@@ -1,1 +1,1 @@
-export const API_SERVER = "https://ba68-219-240-45-12.ngrok-free.app";
+export const API_SERVER = "https://2d3d-219-240-45-12.ngrok-free.app";
---
+++ src/components/content/modal/utils.js
@@ -0,0 +1,15 @@
+export const processApiResponse = (apiResponse) => {
+  return apiResponse.reduce((acc, item) => {
+    acc[item.themeId] = {
+      title: item.themeName,
+      content: item.themeDesc,
+      투자수익률: {
+        "원자재": item.commodityReturn,
+        "코인": item.coinReturn,
+        "주식": item.stockReturn,
+        "부동산": item.realEstateReturn
+      }
+    };
+    return acc;
+  }, {});
+};

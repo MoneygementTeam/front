9a00b8fb9553345c31a210b20ef7ad5903bbff01
Gogoonbuntu <tmddud333@naver.com>
2024-08-24 23:57:09 +0900

    잔디로 바꿈, 하우스캐릭터 추가.

--- src/store/ModalAtom.js
+++ src/store/ModalAtom.js
@@ -4,3 +4,7 @@ export const IsModalOpenAtom = atom({
   key: "IsModalOpenAtom",
   default: false,
 });
+export const RewardPopupAtom = atom({
+  key: 'RewardPopupAtom',
+  default: { isOpen: false, title: '', subTitle: '' },
+});

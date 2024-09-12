5cbcae936831a666b44c98f2d7d882774b549608
Shin seongsu <tlstjdtn321@naver.com>
2024-09-12 21:18:17 +0900

    fix: websocket server 수정

--- src/sockets/clientSocket.js
+++ src/sockets/clientSocket.js
@@ -1,4 +1,3 @@
 import { io } from "socket.io-client";
 
-//export const socket = io("https://danal-angelhack.o-r.kr/");
-export const socket = io("https://moneygement.run.goorm.site");
+export const socket = io("https://danal-angelhack.o-r.kr/");
